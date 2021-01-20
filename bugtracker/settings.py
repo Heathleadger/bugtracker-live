8b45b81e9e9b95849b65519e878e176d58ffb498
Vinicius <viniciu>
2021-01-20 19:13:33 +0100

    Debbug on

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'q+gxt@#147sj@y6_ys#!fgdj7+sgoe2vzw0o0s%--=4bmem$7$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://vsabugtracker.herokuapp.com', '127.0.0.1']
 
